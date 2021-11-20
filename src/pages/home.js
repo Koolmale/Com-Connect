83a5229b43168060b473d105949a8fffb3d41944
okayyyy <zghan2005@gmail.com>
2021-11-19 16:29:24 -0800

    Add carousel

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,12 @@ import MakeAChange from '../images/makeAChange.jpg'
 import AustinFlyer from '../images/austinFlyer.png'
 import Grocery from '../images/grocery.jpg'
 import Donate from '../images/donate.jpg'
+import SriLanka from '../images/sriLanka.jpg'
+import CarePackage from '../images/carePackage.png'
+import Target from '../images/target.jpg'
 
 import ShowcaseCard from '../components/showcaseCard'
-import { CardGroup } from 'react-bootstrap'
+import { CardGroup, Carousel } from 'react-bootstrap'
 
 function Home() {
     return (
@@ -35,124 +38,80 @@ function Home() {
                 </section>
             </main>
 
-        <section id="services">
-            <h2 className='services-header'>What We Do</h2>
+            <section id="services">
+                <h2 className='section-header'>What We Do</h2>
 
-            <CardGroup className='service-list'>
-                <ShowcaseCard 
-                    title="Fundraisers"
-                    description="We run various fundraisers throughout the year. By teaming with many youth groups and organizations, we are able to support those in need."
-                    image={AustinFlyer}
-                    imageAlt="Austin Fundraiser"
-                    buttonText="See more"
-                    buttonLink="/#"
-                />
+                <CardGroup className='service-list'>
+                    <ShowcaseCard 
+                        title="Fundraisers"
+                        description="We run various fundraisers throughout the year. By teaming with many youth groups and organizations, we are able to support those in need."
+                        image={AustinFlyer}
+                        imageAlt="Austin Fundraiser"
+                        buttonText="See more"
+                        buttonLink="/#"
+                    />
 
-                <ShowcaseCard 
-                    title="Grocery"
-                    description="We know the pandemic has impacted many people. With help from many volunteers, we have been able to successfully deliver groceries all over the Bay Area."
-                    image={Grocery}
-                    imageAlt="Grocery"
-                    buttonText="See more"
-                    buttonLink="/#"
-                />
+                    <ShowcaseCard 
+                        title="Grocery"
+                        description="We know the pandemic has impacted many people. With help from many volunteers, we have been able to successfully deliver groceries all over the Bay Area."
+                        image={Grocery}
+                        imageAlt="Grocery"
+                        buttonText="See more"
+                        buttonLink="/#"
+                    />
 
-                <ShowcaseCard 
-                    title="Donate"
-                    description="Donations go towards families that have been affected by the pandemic."
-                    image={Donate}
-                    imageAlt="Donate"
-                    buttonText="Donate Now"
-                    buttonLink="/#"
-                />
+                    <ShowcaseCard 
+                        title="Donate"
+                        description="Donations go towards families that have been affected by the pandemic."
+                        image={Donate}
+                        imageAlt="Donate"
+                        buttonText="Donate Now"
+                        buttonLink="/#"
+                    />
+                    
+                </CardGroup>
                 
-            </CardGroup>
-            
-        </section>
+            </section>
 
-    {/* <div>
-    <h2 class="text-center" >Some of our Previous Projects</h2>
-     <div id="carouselExampleIndicators" class="carousel slide mt-5 mb-5" data-ride="carousel" data-aos="slide-up" data-aos-duration="1750">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="./home/srilanka.jpg" alt="First slide" />
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Care Package Campaign 2020</h5>
-                <p>...</p>
-              </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="./home/carepackage2" alt="Second slide" />
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Care Package Campaign 2020</h5>
-                <p>...</p>
-              </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="./home/target1.jpg" alt="Third slide" />
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Gift Card Fundraiser</h5>
-                <p>...</p>
-              </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-        </div>
-        </div>
+            <h3 className='section-header-small'>Some of our Previous Projects</h3>
 
-        <footer class="text-center text-white" >
-            <div class="container pt-4">
-              <section class="mb-4">
+            <div className='home-carousel-container'>
+                <Carousel classname='home-carousel'>
+                    <Carousel.Item>
+                        <img
+                            src={SriLanka}
+                            alt="Sri Lanka"
+                            className="home-carousel-image"
+                        />
+                        <Carousel.Caption>
+                            <h3>Care Package Campaign 2020</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            src={CarePackage}
+                            alt="Care Package"
+                            className="home-carousel-image"
+                        />
 
-                <a
-                  class="btn btn-link btn-floating btn-lg text-dark m-1"
-                  href="https://www.instagram.com/community_connector/"
-                  role="button"
-                  target="_blank"
-                  rel="noreferrer"
-                  data-mdb-ripple-color="dark"
-                  ><i class="fab fa-2x fa-instagram"></i
-                ></a>
+                        <Carousel.Caption>
+                            <h3>Care Package Campaign 2020</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            src={Target}
+                            alt="Target Gift Cards"
+                            className="home-carousel-image"
+                        />
 
-                <a
-                  class="btn btn-link btn-floating btn-lg text-dark m-1"
-                  href="https://www.linkedin.com/company/com-connector"
-                  role="button"
-                  target="_blank"
-                  rel="noreferrer"
-                  data-mdb-ripple-color="dark"
-                  ><i class="fab fa-2x fa-linkedin" />
-                </a>
-
-                <a
-                  class="btn btn-link btn-floating btn-lg text-dark m-1"
-                  href="https://github.com/Koolmale/Com-Connect"
-                  role="button"
-                  target="_blank"
-                  rel="noreferrer"
-                  data-mdb-ripple-color="dark"
-                  ><i class="fab fa-2x fa-github" />
-                </a>
-              </section>
+                        <Carousel.Caption>
+                            <h3>Gift Card Fundraiser</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
-          
-            <div class="text-center text-dark p-3" >
-              © 2021 Copyright:
-              <a class="text-dark" href="https://comconnector.com/">ComConnector.com</a>
-            </div>
-          </footer> */}
+
         </div>
     )
 }
